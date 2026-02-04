7a9e74906fc3c496aed485134ce1d770c48f8820
Priyank Prajapati <priyankprajapati2609@gmail.com>
2026-02-04 16:08:54 +0530

    update-details

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -35,14 +35,13 @@ const MapSection = () => {
         >
           <div className="map-box">
             <iframe
-              title="Physiotherapy Clinic Location"
-              src="https://www.google.com/maps/place/3J4V%2BP7,+Ahmedabad,+Gujarat/@23.0568174,72.6383219,844m/data=!3m1!1e3!4m13!1m7!3m6!1s0x395e848aba5bd449:0x7136cd41efa2628c!2s3J4V%2BP7,+Ahmedabad,+Gujarat!3b1!8m2!3d23.0568125!4d72.6431875!3m4!1s0x395e848aba5bd449:0x7136cd41efa2628c!8m2!3d23.0568125!4d72.6431875?entry=ttu&g_ep=EgoyMDI2MDIwMS4wIKXMDSoASAFQAw%3D%3D"
-              width="100%"
+              src="https://maps.app.goo.gl/gogaZD6CXr2ZpiiB8"
+              width="600"
               height="450"
-              style={{ border: 0 }}
-              allowFullScreen
+              style="border:0;"
+              allowfullscreen=""
               loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
+              referrerpolicy="no-referrer-when-downgrade"
             />
           </div>
 
